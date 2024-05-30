2f530fd3d1b5699775c0292d0d2a780166e2a271
Hakan Vardar <hakovardo123@gmail.com>
2024-05-30 10:01:52 +0300

    update requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-jm&7z_c)+gc)3*qhw3p(*4_v8wij9ca3s%e@e4y6%iae$lhy7*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
